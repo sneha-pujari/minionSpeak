9d65829242d3cbfc763fd36a95d7dc1afb1e90ab
Sneha Pujari <58720128+sneha-pujari@users.noreply.github.com>
2020-12-20 16:06:13 +0530

    feat - text input from user

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 var btnTranslate = document.querySelector("#btn-translate")
+var txtInput = document.querySelector("#txt-input")
 
-btnTranslate.addEventListener("click", function clickEventHandler(){
-    console.log("Clicked")
-})
+console.log(txtInput)
+function clickEventHandler(){
+    console.log("Clicked!");
+    console.log("Input: ", txtInput);
+}
+btnTranslate.addEventListener("click", clickEventHandler())
